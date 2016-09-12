2e12d7ee567a3d0ea813270ba69aa14175afd9f8
pirog.v <pirog.v@gmail.com>
2016-09-12 10:44:27 +0300

    migrations

---
+++ database/migrations/2016_09_12_073605_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+			$table->increments('id');
+			$table->bigInteger('account_id');
+			$table->bigInteger('post_id');
+			$table->string('title', 225);
+			$table->string('datetime', 225);
+			$table->string('description', 225);
+			$table->integer('num_favorites');
+			$table->integer('num_replies');
+			$table->integer('num_retweets');
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
